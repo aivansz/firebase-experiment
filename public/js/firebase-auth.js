b8c88d4f420b93aa0c775cbc551081c2413443c0
Ivan <ivan.silva@multivarejogpa.com.br>
2018-07-20 17:58:51 -0300

    firebase integration

--- public/js/firebase-auth.js
+++ public/js/firebase-auth.js
@@ -8,20 +8,41 @@ const getInputValue = function(element){
     return selector(element).value;   
 }
 
-console.log('code');
-
 getInputValue('#send-button');
 
 sendButton.addEventListener('click', function(event){
-    console.log('clicked', event);
     let login = getInputValue('#login-input');
     let password = getInputValue('#password-input');
-    console.log(login);
-    console.log(password);
+    loginFirebase(login, password);
 });
 
 
 
-/*const login = function(login, password){
+const loginFirebase = function(login, password){
 
-}*/
+    firebase.auth()
+        .signInWithEmailAndPassword(login, password)
+        .catch(function(error){
+            console.log(error.code);
+            console.log(error.message);
+        });
+
+        firebase.auth().onAuthStateChanged(function(user) {
+            if (user) {
+              // User is signed in.
+              var displayName = user.displayName;
+              var email = user.email;
+              var emailVerified = user.emailVerified;
+              var photoURL = user.photoURL;
+              var isAnonymous = user.isAnonymous;
+              var uid = user.uid;
+              var providerData = user.providerData;
+              console.log(user);
+              // ...
+            } else {
+                console.log('usuário não encontrado');
+            }
+          });
+
+}
+
