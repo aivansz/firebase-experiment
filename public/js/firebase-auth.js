a3f2fe0a2b8e146582f0bf151f2f15ccee205d08
Ivan <ivan.silva@multivarejogpa.com.br>
2018-07-23 16:41:44 -0300

    login finished

--- public/js/firebase-auth.js
+++ public/js/firebase-auth.js
@@ -16,8 +16,6 @@ sendButton.addEventListener('click', function(event){
     loginFirebase(login, password);
 });
 
-
-
 const loginFirebase = function(login, password){
 
     firebase.auth()
@@ -25,24 +23,26 @@ const loginFirebase = function(login, password){
         .catch(function(error){
             console.log(error.code);
             console.log(error.message);
-        });
+    });
 
-        firebase.auth().onAuthStateChanged(function(user) {
-            if (user) {
-              // User is signed in.
-              var displayName = user.displayName;
-              var email = user.email;
-              var emailVerified = user.emailVerified;
-              var photoURL = user.photoURL;
-              var isAnonymous = user.isAnonymous;
-              var uid = user.uid;
-              var providerData = user.providerData;
-              console.log(user);
-              // ...
-            } else {
-                console.log('usuário não encontrado');
-            }
-          });
+    irebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+          // User is signed in.
+          var displayName = user.displayName;
+          var email = user.email;
+          var emailVerified = user.emailVerified;
+          var photoURL = user.photoURL;
+          var isAnonymous = user.isAnonymous;
+          var uid = user.uid;
+          var providerData = user.providerData;
+          //console.log('onAuth', user);
+          //console.log('Login!!!!');
+          
+          // ...
+        } else {
+            console.log('usuário não encontrado');
+        }
+      });
 
 }
 
